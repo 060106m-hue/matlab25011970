9195d52055fe0bcde85d51ad8c8f38f92044ef36
060106m-hue <060106m@gmail.com>
2026-03-07 00:12:50 +0900

    444

--- FileName.c
+++ FileName.c
@@ -1,8 +1,5 @@
-#pragma warning(disable:4996)
-#include <stdio.h>
-#include <stdlib.h>// rand 함수 사용을 위해
-#include <time.h> // clock() 함수 사용을 위해
-int main(void) {
-	printf("helvvvv");
+#include<stdio.h>
+int main() {
+	printf("hello");
 	return 0;
 }
